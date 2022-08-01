1e4258edce4bf0de32084893e4d2957a28809a9e
official-simple-soul <abdurroheemsherifdeen@gmail.com>
2022-08-01 14:07:56 +0100

    portfolio update

--- js/app.js
+++ js/app.js
@@ -111,6 +111,29 @@ const worksDetails = [
     }
 ]
 
+const successStory = [
+    {
+        image: '<i class="fa-solid fa-hourglass-end fs-2"></i>',
+        title: '200',
+        text: 'Completed project',
+    },
+    {
+        image: '<i class="fa-solid fa-face-laugh fs-2"></i>',
+        title: '150',
+        text: 'Happy clients',
+    },
+    {
+        image: '<i class="fa-solid fa-code fs-2"></i>',
+        title: '10k',
+        text: 'Line codes written',
+    },
+    {
+        image: '<i class="fa-solid fa-spinner fs-2"></i>',
+        title: '30',
+        text: 'Projects in progress',
+    },
+]
+
 
 let rowWrap = document.querySelector('.row-wrap');
 let rowEdu = document.querySelector('.row-edu');
@@ -120,6 +143,7 @@ let numberTwo = document.querySelector('.number-two');
 let numberThree = document.querySelector('.number-three');
 let numberFour = document.querySelector('.number-four');
 let workRow = document.querySelector('.work-row');
+let successStoryRow = document.querySelector('.success-story-row');
 
 let count1 = 0;
 let count2 = 0;
@@ -131,7 +155,7 @@ let count4 = 0;
 for (let i = 0; i < aboutText.length; i++) {
     let aboutItem = ''
     aboutItem += `  <div class="col-md-4">
-                        <div class="card pt-3 my-3 bg-transparent">
+                        <div class="card pt-3 my-3 bg-transparent border-0 shadow">
                             <img src="${aboutText[i].image}" alt="" class="w-25 mx-auto">
                             <div class="card-body">
                                 <h6 class="fw-bold">${aboutText[i].title}</h6>
@@ -245,6 +269,22 @@ function getWork() {
     workRow.innerHTML = works;
 }
 getWork();
+
+// success story
+for (let i = 0; i < successStory.length; i++) {
+    let successStoryItem = ''
+    successStoryItem += `   <div class="col-md-3">
+                                <div class="card border-0 border-5 border-secondary border-top border-start shadow bg-transparent text-center py-3">
+                                    <div class="d-flex justify-content-center align-items-center">
+                                        ${successStory[i].image}
+                                        <h2 class="mx-4 fw-bold mb-0">${successStory[i].title}</h2>
+                                    </div>
+                                    <h5 class="pt-3">${successStory[i].text}</h5>
+                                </div>
+                            </div>`
+    successStoryRow.innerHTML += successStoryItem;
+}
+
 
 
 
